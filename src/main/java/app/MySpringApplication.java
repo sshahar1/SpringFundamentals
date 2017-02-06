6fd5cebb8f72fa76c2d113a0b0440bc94204c58c
sigals <sigals@tikalk.com>
2017-02-06 17:41:48 +0200

    Properties and swagger

--- src/main/java/app/MySpringApplication.java
+++ src/main/java/app/MySpringApplication.java
@@ -1,14 +1,24 @@
 package app;
 
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by sigals on 26/09/2016.
  */
 @SpringBootApplication
+@EnableSwagger2
+@Api(value = "MyController", description = "Spring workshop")
 @ComponentScan(basePackages = "controller")
 public class MySpringApplication {
 
@@ -20,4 +30,27 @@ public class MySpringApplication {
     public String myString() {
         return "Sigal";
     }
+
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = ApiInfo.DEFAULT;
+        return apiInfo;
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfiguration.DEFAULT;
+    }
+
 }
--- src/main/java/controller/MyRestController.java
+++ src/main/java/controller/MyRestController.java
@@ -2,6 +2,7 @@ package controller;
 
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,31 @@ import javax.inject.Inject;
 @Api(value = "workshop", description = "Workshop")
 public class MyRestController {
 
+    private String sigals1;
+
+    @Value("${sigals}")
+    private String sigals2;
+
+    public MyRestController(@Value("${sigals}") String sigals) {
+        this.sigals1 = sigals;
+    }
+
     @Inject
     @Qualifier("rivki")
     private String myString;
 
-    @RequestMapping(value ="/getrivki", method = RequestMethod.GET)
+    @RequestMapping(value ="/getrivki", method = RequestMethod.GET, produces = "text/plain")
     public String getRivki() {
         return myString;
+    }
+
+    @RequestMapping(value = "/propertyConstructor", method = RequestMethod.GET, produces = "text/plain")
+    public String getPropertyConstructor() {
+        return sigals1;
+    }
+
+    @RequestMapping(value = "/propertyAnnotation", method = RequestMethod.GET, produces = "text/plain")
+    public String getPropertyAnnotation() {
+        return sigals2;
     }
 }
